31b0e69df8c362e9779064b927867c4659c8bcae
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:04:59 +0500

    Add files via upload
    assignment_16

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
-"use strict";
-let Guest_list = [`Rohan`, `fatima`, `salar`];
-for (let i = 0; i < Guest_list.length; i++) {
-    console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou `);
+var Guest_list = ["Rohan", "fatima", "salar"];
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr." + Guest_list[i] + ",\n\nIt my pleasure to invite you in our dinner\n\n ThankYou ");
 }
-let absent_Guest = `Rohan`;
-let new_Guest = `NABEEL`;
+var absent_Guest = "Rohan";
+var new_Guest = "NABEEL";
 Guest_list[0] = new_Guest;
-for (let i = 0; i < Guest_list.length; i++) {
-    console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou\n\n `);
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr." + Guest_list[i] + ",\n\nIt my pleasure to invite you in our dinner\n\n ThankYou\n\n ");
 }
-console.log(`MR ${absent_Guest} is not coming in the dinner `);
-console.log(`Gos News! we fine a big table so we are inviting a 3 more guest`);
-Guest_list.unshift(`sir zia khan`);
-Guest_list.splice(2, 0, `KAFEEL AHMED`);
-Guest_list.push(`SAAD SALAR`);
-for (let i = 0; i < Guest_list.length; i++) {
-    console.log(`Dear Mr.` + Guest_list[i] + `, \n\n It my pleasure to invite you in our dinner\n\n ThankYou \n\n `);
+console.log("MR ".concat(absent_Guest, " is not coming in the dinner "));
+console.log("Gos News! we fine a big table so we are inviting a 3 more guest");
+Guest_list.unshift("sir zia khan");
+Guest_list.splice(2, 0, "KAFEEL AHMED");
+Guest_list.push("SAAD SALAR");
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr." + Guest_list[i] + ", \n\n It my pleasure to invite you in our dinner\n\n ThankYou \n\n ");
 }
+console.log("\nsorry we can not arrange big table only two people will be invited");
+while (Guest_list.length > 2) {
+    var remove_Guest = Guest_list;
+    console.log("Dear Mr. ".concat(remove_Guest, " You are still not invited for dinner"));
+}
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr.  " + Guest_list[i] + "n\n You are still invited. n\n Thankyou n\n ");
+}
+;
+// I remove all guest
+Guest_list.splice(0, 2);
+console.log(Guest_list);
--- app.ts
+++ app.ts
@@ -3,11 +3,6 @@ for(let i=0; i<Guest_list.length; i++){
     console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou `)
 }
 
-
-
-
-
-
 let absent_Guest : string = `Rohan`
 
 let new_Guest : string = `NABEEL`
@@ -18,9 +13,7 @@ for(let i=0; i<Guest_list.length; i++){
     console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou\n\n `)
 
 }
-
 console.log(`MR ${absent_Guest} is not coming in the dinner `)
-
 console.log(`Gos News! we fine a big table so we are inviting a 3 more guest`);
 
 Guest_list. unshift(`sir zia khan`);
@@ -31,6 +24,19 @@ for (let i=0; i<Guest_list.length; i++){
     console.log(`Dear Mr.` + Guest_list[i] +`, \n\n It my pleasure to invite you in our dinner\n\n ThankYou \n\n `)
 }
 
+console.log(`\nsorry we can not arrange big table only two people will be invited`);
+while(Guest_list.length > 2){
+    let remove_Guest = Guest_list
+    console.log(`Dear Mr. ${remove_Guest} You are still not invited for dinner`)
+}
+for(let i=0; i<Guest_list.length; i++){
+    console.log(`Dear Mr.  ` + Guest_list[i] + `n\n\ You are still invited. n\n\ Thankyou n\n `)
+    
+};
+
+// I remove all guest
+Guest_list.splice(0,2);
+console.log(Guest_list);
 
 
 
