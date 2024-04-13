ab338a63a85a17e4c2c27c634a87da9e7a4f8431
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:12:54 +0500

    Add files via upload
    assignment_24

--- app.js
+++ app.js
@@ -1,10 +1,27 @@
-var book = {
-    name: "ESSENTIALTYPESCRIPT",
-    price: 450
-};
-var mobile = {
-    name: "OPPO",
-    Price: 1000
-};
-console.log("book name: ".concat(book.name, ", book price $").concat(book.price));
-console.log("mobile.name: ".concat(mobile.name, ", mobile price $").concat(mobile.price));
+// Equality and inequality test
+console.log("Equality test with string:", "Apple" === "Apple");
+// Equality and Inequality test
+console.log("Inequality test with string:", "Apple" != "Orage");
+// test using the lower case funtion
+console.log("lower case funtion test:", "HELLO".toLowerCase() === "hello");
+// Numerical test using equality and inequality
+console.log("Equality test with number:", 26 === 26);
+// numerical test involving inequality
+console.log("Inequality test with number:", 26 != 35);
+// , greater than test,
+console.log("Greater than test:", 10 > 5);
+// less than test
+console.log("Less than test :", 5 < 10);
+// greater than or less than test, 
+console.log("Greater than or equal to test:", 10 >= 10);
+// and less than equal to
+console.log("Less than or equal to test:", 5 <= 10);
+// test using "and" operators
+console.log("And operator test:", 5 === 5 && 10 > 5);
+// test using  "or" operators
+console.log("Or operator test:", 5 === 5 || false);
+// test whether an item is in a arry
+var fruits = ["mango", "apple", "banana"];
+console.log("test \"mango\" in a arry:", fruits.includes("mango"));
+// test whether an item is not in arry
+console.log("test \"Nashpati\" is not in arry:", !fruits.includes("Nashpati"));
