2085afd1fac33ce0e8ff08d3224ab73663c4dd80
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:17:15 +0500

    Add files via upload
    assignment_30

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
-var favourite_fruits = ['apple', 'banana', 'mango'];
-if (favourite_fruits.includes('apple')) {
-    console.log('I really like apply');
-}
-if (favourite_fruits.includes('mango')) {
-    console.log('I really like mango');
-}
-if (favourite_fruits.includes('banana')) {
-    console.log('I really like banana');
-}
-if (favourite_fruits.includes('strawberry')) {
-    console.log('I really like strawberry');
-}
-if (favourite_fruits.includes('pear')) {
-    console.log('I really like pear');
+var users = ['admin', 'eric', 'jackson', 'fatima', 'sana'];
+for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
+    var user = users_1[_i];
+    if (user === 'admin') {
+        console.log("hello admin,would you like to see a status report");
+    }
+    else {
+        console.log('hello ${users},thankyou forlogging in again');
+    }
 }
--- app.ts
+++ app.ts
@@ -1,17 +1,11 @@
-let favourite_fruits: string [] = ['apple','banana','mango'];
+let users: string[] = ['admin', 'mike','jackson','fatima','eric']
 
-if (favourite_fruits.includes('apple')){
-    console.log('I really like apply');
-}
-if (favourite_fruits.includes('mango')){
-    console.log('I really like mango');
-}
-if (favourite_fruits.includes('banana')){
-    console.log('I really like banana');
-}
-if (favourite_fruits.includes('strawberry')){
-    console.log('I really like strawberry');
-}
-if (favourite_fruits.includes('pear')){
-    console.log('I really like pear');
+
+for (let user of users){
+    if (user === 'admin'){
+        console.log("hello admin,would you like to see a status report")
+    }
+    else{
+        console.log('hello ${users},thankyou forlogging in again')
+    }
 }
