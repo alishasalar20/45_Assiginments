e128ad982228609c03de1c9f9feb14fee75c95f6
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:17:47 +0500

    Add files via upload
    assignment_31

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
-var users = ['admin', 'eric', 'jackson', 'fatima', 'sana'];
-for (var _i = 0, users_1 = users; _i < users_1.length; _i++) {
-    var user = users_1[_i];
-    if (user === 'admin') {
-        console.log("hello admin,would you like to see a status report");
-    }
-    else {
-        console.log('hello ${users},thankyou forlogging in again');
-    }
+var users = [];
+if (users.length === 0) {
+    console.log("we need to fund some users! ");
+}
+else {
+    users = [];
+    console.log("All users have been removed" + users.length);
 }
--- app.ts
+++ app.ts
@@ -1,11 +1,8 @@
-let users: string[] = ['admin', 'mike','jackson','fatima','eric']
-
-
-for (let user of users){
-    if (user === 'admin'){
-        console.log("hello admin,would you like to see a status report")
-    }
-    else{
-        console.log('hello ${users},thankyou forlogging in again')
-    }
+let users: string [] = []
+if (users.length === 0){
+    console.log("we need to fund some users! ");
+}
+else{
+    users = [];
+    console.log("All users have been removed" + users.length)
 }
