1852dcfee1a1ef999428ca0983e87814757e0cd1
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:14:04 +0500

    Add files via upload
    assignment_26

--- app.js
+++ app.js
@@ -1,27 +1,11 @@
-// Equality and inequality test
-console.log("Equality test with string:", "Apple" === "Apple");
-// Equality and Inequality test
-console.log("Inequality test with string:", "Apple" != "Orage");
-// test using the lower case funtion
-console.log("lower case funtion test:", "HELLO".toLowerCase() === "hello");
-// Numerical test using equality and inequality
-console.log("Equality test with number:", 26 === 26);
-// numerical test involving inequality
-console.log("Inequality test with number:", 26 != 35);
-// , greater than test,
-console.log("Greater than test:", 10 > 5);
-// less than test
-console.log("Less than test :", 5 < 10);
-// greater than or less than test, 
-console.log("Greater than or equal to test:", 10 >= 10);
-// and less than equal to
-console.log("Less than or equal to test:", 5 <= 10);
-// test using "and" operators
-console.log("And operator test:", 5 === 5 && 10 > 5);
-// test using  "or" operators
-console.log("Or operator test:", 5 === 5 || false);
-// test whether an item is in a arry
-var fruits = ["mango", "apple", "banana"];
-console.log("test \"mango\" in a arry:", fruits.includes("mango"));
-// test whether an item is not in arry
-console.log("test \"Nashpati\" is not in arry:", !fruits.includes("Nashpati"));
+var alien_Color = "yellow";
+if (alien_Color === "yellow") {
+    console.log("the player just earned 5 points for shooting the alien");
+}
+else {
+    console.log('the player just earned 10 points');
+}
+alien_Color = "red";
+if (alien_Color === "yellow") {
+    console.log("the player just earned 5 points for shooting the alien");
+}
--- app.ts
+++ app.ts
@@ -1,29 +1,14 @@
-// Equality and inequality test
-console.log("Equality test with string:", "Apple" === "Apple");
+let alien_Color: string = "yellow"
 
-// Equality and Inequality test
-console.log("Inequality test with string:", ("Apple" as string) != "Orage")
+if(alien_Color === "yellow"){
+    console.log("the player just earned 5 points for shooting the alien");
+}
 
-// test using the lower case funtion
-console.log("lower case funtion test:", "HELLO".toLowerCase() === "hello");
-// Numerical test using equality and inequality
-console.log("Equality test with number:", 26 === 26);
-// numerical test involving inequality
-console.log("Inequality test with number:", (26 as number) != 35);
-// , greater than test,
-console.log("Greater than test:", 10 > 5);
-// less than test
-console.log("Less than test :", 5 < 10);
-// greater than or less than test, 
-console.log("Greater than or equal to test:", 10 >= 10);
-// and less than equal to
-console.log("Less than or equal to test:", 5 <= 10);
-// test using "and" operators
-console.log("And operator test:", 5===5 && 10 > 5);
-// test using  "or" operators
-console.log("Or operator test:", 5===5 || false);
-// test whether an item is in a arry
-const fruits : string [] = [`mango`, `apple`,`banana`]
-console.log(`test "mango" in a arry:`, fruits.includes("mango"));
-// test whether an item is not in arry
-console.log(`test "Nashpati" is not in arry:`, !fruits.includes("Nashpati"));
+else{
+    console.log('the player just earned 10 points')
+}
+
+alien_Color = "red"
+if(alien_Color === "yellow"){
+    console.log("the player just earned 5 points for shooting the alien")
+}
