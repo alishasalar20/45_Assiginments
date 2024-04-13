953e97736b1e7b5cffc4fcc3de042b6d02a24fe5
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:12:13 +0500

    Add files via upload
    assignment_23

--- main.js
+++ main.js
@@ -1,5 +1,31 @@
-var newArray = ["sir zia khan", "sir ameen", "sir ahmed"];
-// Accessing arry by index
-console.log(newArray[5]);
-// correct the error
-console.log(newArray);
+var car = "subaru";
+// test 1 Equality comparsion
+console.log("Is car == `subaru` I predict true");
+console.log(car == "subaru");
+// test 2 strict equality comparsion
+console.log("Is car === `subaru` I predict true");
+console.log(car === "subaru");
+// test 3 Inequality comparsion
+console.log("Is car != `subaru` I predict false. ");
+console.log(car != "subaru");
+// test 4 strict inequality comparsion
+console.log("Is car !== `subaru` I predict false");
+console.log(car !== "subaru");
+// test 5 less than comparsion
+console.log("Is car< `subaru` I predict false.");
+console.log(car < "subaru");
+// test 6 greater than comparsion
+console.log("Is car > `subaru` I predict false");
+console.log(car > "subaru");
+// test 7 less than or equal comparsion
+console.log("Is car <= `subaru` I predict true.");
+console.log(car <= "subaru");
+// test 8 greater than or equal comparion
+console.log("Is car >= `subaru` I predict true.");
+console.log(car >= "subaru");
+// test 9 checking truthiness
+console.log("Is car? `subaru` I predict true.");
+console.log(car);
+// test 10 checking falsiness
+console.log("Is Icar? I predict false.");
+console.log(car);
--- main.ts
+++ main.ts
@@ -1,7 +1,40 @@
-let newArray: string[] = [`sir zia khan`, `sir ameen`, `sir ahmed`];
+let car: string = "subaru";
+// test 1 Equality comparsion
+console.log("Is car == `subaru` I predict true")
+console.log(car == `subaru`);
 
-// Accessing arry by index
-console.log(newArray[5])
+// test 2 strict equality comparsion
+console.log("Is car === `subaru` I predict true");
+console.log(car === `subaru`)
 
-// correct the error
-console.log(newArray)
+// test 3 Inequality comparsion
+console.log("Is car != `subaru` I predict false. ");
+console.log(car != `subaru`)
+
+// test 4 strict inequality comparsion
+console.log("Is car !== `subaru` I predict false");
+console.log(car !== `subaru`);
+
+// test 5 less than comparsion
+console.log("Is car< `subaru` I predict false.");
+console.log(car< `subaru`);
+
+// test 6 greater than comparsion
+console.log("Is car > `subaru` I predict false");
+console.log(car > `subaru`);
+
+// test 7 less than or equal comparsion
+console.log("Is car <= `subaru` I predict true.");
+console.log(car <= `subaru`);
+
+// test 8 greater than or equal comparion
+console.log("Is car >= `subaru` I predict true.");
+console.log(car >= `subaru`);
+
+// test 9 checking truthiness
+console.log("Is car? `subaru` I predict true.");
+console.log(car)
+
+// test 10 checking falsiness
+console.log("Is Icar? I predict false.");
+console.log(car)
