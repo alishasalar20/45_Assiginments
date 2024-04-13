abfa31209acf7341aefbd0cbb3d633d3b6261572
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:01:02 +0500

    Add files via upload
    assignment_11

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-var members = ["amna", "muniba", "rohan,", "alisha"];
-for (var i = 0; i < members.length; i++)
-    ;
-{
-    console.log(members[i++]);
+var members = ["alisha", "rohan", "eshah", "laiba"];
+var message = "would you like to spend this ramadan with us";
+for (var i = 0; i < members.length; i++) {
+    console.log(message + members[i]);
 }
--- app.ts
+++ app.ts
@@ -1,4 +1,5 @@
-let members: string[] =[`amna`,`muniba`,`rohan,`,`alisha`];
-for(let i=0; i<members.length; i++);{
-    console.log(members[i]);
+let members: string[] =[`alisha`,`rohan`,`eshah`,`laiba`];
+let message: string=`would you like to spend this ramadan with us`;
+for (let i=0; i<members.length; i++){
+    console.log(message + members[i]);
 }
