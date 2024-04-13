71ef0c7ece78b5e8e1120b6e8e4d7383a0c57811
alishasalar20 <166957393+alishasalar20@users.noreply.github.com>
2024-04-14 03:02:29 +0500

    Add files via upload
    assignment_13

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-var transportation = ["bike", "buses", "cars", "cycle"];
-for (var i = 0; i < transportation.length; i++)
-    ;
-{
-    console.log("I Would like to own  a " + transportation[i]);
+var Guest_list = ["Rohan", "fatima", "salar"];
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr." + Guest_list[i] + ",\n\nIt my pleasure to invite you in our dinner\n\n ThankYou ");
 }
+var absent_Guest = "Rohan";
+var new_Guest = "NABEEL";
+Guest_list[0] = new_Guest;
+for (var i = 0; i < Guest_list.length; i++) {
+    console.log("Dear Mr." + Guest_list[i] + ",\n\nIt my pleasure to invite you in our dinner\n\n ThankYou\n\n ");
+}
+console.log("MR ".concat(absent_Guest, " is not coming in the dinner "));
--- app.ts
+++ app.ts
@@ -1,6 +1,25 @@
-let transportation: string[] = [`bike`,`buses`,`cars`,`cycle`];
-
-for(let i=0; i<transportation.length; i++);{
-
-    console.log(`I Would like to own  a ` + transportation[i])
+let Guest_list: string[] = [ `Rohan`,`fatima`,`salar`];
+for(let i=0; i<Guest_list.length; i++){
+    console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou `)
 }
+
+
+
+
+
+
+let absent_Guest : string = `Rohan`
+
+let new_Guest : string = `NABEEL`
+
+Guest_list[0] = new_Guest 
+
+for(let i=0; i<Guest_list.length; i++){
+    console.log(`Dear Mr.` + Guest_list[i] + `,\n\nIt my pleasure to invite you in our dinner\n\n ThankYou\n\n `)
+
+}
+
+console.log(`MR ${absent_Guest} is not coming in the dinner `)
+
+
+
